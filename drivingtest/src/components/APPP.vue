9fb6d773d7d8162918280326fc98de39daadd719
weiming0731 <71959919+weiming0731@users.noreply.github.com>
2020-09-29 08:57:27 +0800

    倒计时器

---
+++ drivingtest/src/components/APPP.vue
@@ -0,0 +1,21 @@
+<template>
+     <div>
+         <zk-time-down @time-end="message = '倒计时结束'" :endTime='endTime'></zk-time-down>
+         <p>{{message}}</p> 
+    </div>
+</template>
+
+<script>
+import zkTimeDown from './components/zkTimeDown/zkTimeDown.vue'
+export default {
+    components : {
+        zkTimeDown
+    },
+    data () {
+        return {
+            message : '正在倒计时',
+            endTime : '2017-04-08 10:06:00'
+        }
+    }
+}
+</script>
